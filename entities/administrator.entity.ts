b689119bf39a9e081718c66b0e154bef913816ce
Milic Vojinovic <milic.vojinovic@gmail.com>
2020-07-09 17:57:34 +0200

    Implement a global  API response mechanism

--- entities/administrator.entity.ts
+++ entities/administrator.entity.ts
@@ -2,7 +2,7 @@ import { Column, Entity, Index, PrimaryGeneratedColumn } from "typeorm";
 
 @Index("uq_administrator_username", ["username"], { unique: true })
 @Entity("administrator")
-export class Administrator {
+export class  Administrator {
   @PrimaryGeneratedColumn({
     type: "int",
     name: "administrator_id",
