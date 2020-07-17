af79293c725ebb505bd54f2a0a45a4e6bd72e7f0
Milic Vojinovic <milic.vojinovic@gmail.com>
2020-07-17 12:26:38 +0200

    Create complex DTO

--- src/services/article/article.service.ts
+++ src/services/article/article.service.ts
@@ -3,12 +3,59 @@ import { TypeOrmCrudService } from "@nestjsx/crud-typeorm";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { Article } from "entities/article.entity";
+import { AddArticleDto } from "src/dtos/article/add.article.dto";
+import { ApiResponse } from "src/misc/api.response.class";
+import { ArticlePrice } from "entities/article-price.entity";
+import { ArticleFeature } from "entities/article-feature.entity";
 
 @Injectable()
 export class ArticleService extends TypeOrmCrudService<Article>{
     constructor (
-        @InjectRepository(Article) private readonly article: Repository<Article> //  moramo evidentirati u app module!!!!
-        ) {
+        @InjectRepository(Article) private readonly article: Repository<Article> ,//  moramo evidentirati u app module!!!!
+         
+
+        @InjectRepository(ArticlePrice) private readonly articlePrice: Repository<ArticlePrice>,//  moramo evidentirati u app module!!!!
+      
+
+        @InjectRepository(ArticleFeature) private readonly articleFeature: Repository<ArticleFeature>//  moramo evidentirati u app module!!!!
+        ) 
+        
+        {
         super(article);
+    }
+
+    async createFullArticle (data: AddArticleDto) : Promise<Article | ApiResponse >{
+        let newArticle : Article = new Article();
+        newArticle.name =  data.name;
+        newArticle.categoryId = data.categoryId;
+        newArticle.excerpt = data.excerpt;
+        newArticle.description = data.description;
+
+        let savedArticle  = await this.article.save(newArticle);
+
+        let newArticlePrice : ArticlePrice = new ArticlePrice();
+        newArticlePrice.articleId = savedArticle.articleId;
+        newArticlePrice.price = data.price;
+
+        await this.articlePrice.save(newArticlePrice);
+
+        for (let feature of data.features){
+            let newArticleFeature = new ArticleFeature();
+            newArticleFeature.articleId = savedArticle.articleId;
+            newArticleFeature.featureId = feature.featureId;
+            newArticleFeature.value = feature.value;
+
+            await this.articleFeature.save(newArticleFeature);
+        }
+
+        return await this.article.findOne(savedArticle.articleId , {
+            relations: [
+                 "category",
+                 "articleFeatures",
+                 "features",
+                 "articlePrices"
+            ]
+        })
+
     }
 }
