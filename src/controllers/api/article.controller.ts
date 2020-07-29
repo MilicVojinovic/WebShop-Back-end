acbe9d0beaca2db9193701a4c3c2fc1b6655256f
Milic Vojinovic <milic.vojinovic@gmail.com>
2020-07-29 14:42:52 +0200

    Implement a mechanism to delete article photos

--- src/controllers/api/article.controller.ts
+++ src/controllers/api/article.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, UseInterceptors, Param, UploadedFile, Req } from "@nestjs/common";
+import { Controller, Post, Body, UseInterceptors, Param, UploadedFile, Req, Delete } from "@nestjs/common";
 import { Crud } from "@nestjsx/crud";
 import { Article } from "src/entities/article.entity";
 import { ArticleService } from "src/services/article/article.service";
@@ -138,9 +138,9 @@ export class ArticleController {
 
         }
 
-        await this.createResizedImage(photo ,StorageConfig.photo.resize.thumb );
+        await this.createResizedImage(photo, StorageConfig.photo.resize.thumb);
 
-        await this.createResizedImage(photo ,StorageConfig.photo.resize.small );
+        await this.createResizedImage(photo, StorageConfig.photo.resize.small);
 
         const newPhoto: Photo = new Photo();
 
@@ -156,7 +156,7 @@ export class ArticleController {
         return savedPhoto;
     }
 
-  
+
 
     async createResizedImage(photo, resizeSettings) {
 
@@ -173,6 +173,45 @@ export class ArticleController {
                 height: resizeSettings.height,
             })
             .toFile(destinationFilePath);
+    }
+
+    // http://localhost:3000/api/article/1/deletePhoto/45/
+    @Delete(':articleId/deletePhoto/:photoId')
+    public async deletePhoto(
+        @Param('articleId') articleId: number,
+        @Param('photoId') photoId: number) {
+
+        const photo = await this.photoService.findOne({
+            articleId: articleId,
+            photoId: photoId
+        });
+
+        if (!photo) {
+            return new ApiResponse('error', -4004, 'Photo not found')
+        }
+        try{
+        fs.unlinkSync(StorageConfig.photo.destination + photo.imagePath);
+        fs.unlinkSync(StorageConfig.photo.destination
+            + StorageConfig.photo.resize.thumb.directory
+            + photo.imagePath);
+        fs.unlinkSync(StorageConfig.photo.destination
+            + StorageConfig.photo.resize.small.directory
+            + photo.imagePath);
+        }  catch (e) {}
+
+
+        const deleteResult = await this.photoService.deleteById(photoId);
+
+        if(deleteResult.affected === 0){
+            return new ApiResponse('error', -4004, 'Photo not found')
+        }
+
+        return new ApiResponse('ok' , 0 , 'One photo deleted');
+
+
+
+
+
     }
 
 
