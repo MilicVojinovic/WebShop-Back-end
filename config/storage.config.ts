f204ebfc0908961afaf1e64fc3e9cb6d46560b97
Milic Vojinovic <milic.vojinovic@gmail.com>
2020-08-03 14:45:22 +0200

    Implement statically served photos

--- config/storage.config.ts
+++ config/storage.config.ts
@@ -1,6 +1,8 @@
 export const StorageConfig = {
     photo: {
         destination: '../storage/photos/',
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 dana traje kesirana slika
+        urlPrefix: '/assets/photos', // localhost:3000/urlPrefix gets us in folder destination (  ../storage/photos/  )
         maxSize: 3 * 1024 * 1024,  // in bytes = 3 MB
         resize: {
             thumb: {
