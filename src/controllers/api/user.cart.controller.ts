34a5b0d181ae250f4297b348d2fc4a6f5e07719c
Milic Vojinovic <milic.vojinovic@gmail.com>
2020-08-05 13:29:42 +0200

    Implement the  mechanism to make orders

--- src/controllers/api/user.cart.controller.ts
+++ src/controllers/api/user.cart.controller.ts
@@ -10,14 +10,24 @@ import { AddArticleDto } from "src/dtos/article/add.article.dto";
 import { AddArticleToCartDto } from "src/dtos/cart/add.article.to.cart.dto";
 import { EditArticleDto } from "src/dtos/article/edit.article.dto";
 import { EditArticleInCartDto } from "src/dtos/cart/edit.article.in.cart.dto";
+import { Order } from "src/entities/order.entity";
+import { OrderService } from "src/services/order/order.service";
+import { ApiResponse } from "src/misc/api.response.class";
 
 @Controller('api/user/cart')
 export class UserCartController {
     constructor(
-        private cartService: CartService
+        // use Cart Service 
+        private cartService: CartService,
+
+        // use Order Service 
+        private orderService: OrderService
+
+
     ) { }
 
 
+    // check if there already exist cart for active user; if not create new cart 
     private async getActiveCartForUserId (userId : number) : Promise<Cart>{
 
         let cart = await this.cartService.getLastActiveCartByUserId(userId);
@@ -36,6 +46,7 @@ export class UserCartController {
     @UseGuards(RoleCheckerGuard)
     @AllowToRoles('user')
     async getCurrentCart(@Req() req: Request): Promise<Cart> {
+        // get cart for user based on his id from token  
         return await this.getActiveCartForUserId(req.token.id);
       }
 
@@ -44,9 +55,12 @@ export class UserCartController {
     @Post('addToCart')
     @UseGuards(RoleCheckerGuard)
     @AllowToRoles('user')
-    async addToCart(@Body() data:AddArticleToCartDto,@Req() req: Request) : Promise<Cart> {
+
+    // 
+    async addToCart (@Body() data:AddArticleToCartDto,@Req() req: Request) : Promise<Cart> {
+        // get cart for user based on his id from token  
         const cart = await this.getActiveCartForUserId(req.token.id);
-        
+        // add article to cart or if already exist in cart change his quantity value 
         return await this.cartService.addArticleToCart(cart.cartId , data.articleId , data.quantity);
     }
 
@@ -55,9 +69,27 @@ export class UserCartController {
     @UseGuards(RoleCheckerGuard)
     @AllowToRoles('user')
     async changeQuantity ( @Body() data:EditArticleInCartDto ,@Req() req: Request) : Promise<Cart>{
+        // get cart for user based on his id from token 
         const cart = await this.getActiveCartForUserId(req.token.id);
+        // change article quantity and delete article from cart if quantity value is set to 0
         return await this.cartService.changeQuantity(cart.cartId , data.articleId , data.quantity);
+    }
+
+
+
+    // POST http://localhost:3000/api/user/cart/makeOrder/
+    @Post('makeOrder')
+    @UseGuards(RoleCheckerGuard)
+    @AllowToRoles('user')
+    async makeOrder(@Req() req: Request): Promise<Order | ApiResponse> {
+
+        // get cart for user based on his id from token 
+        const cart = await this.getActiveCartForUserId(req.token.id);
+
+        return await this.orderService.add(cart.cartId);
         
     }
+
+
 
 }
