3ff6f0d2dc45c4b7ca73bd4e336fda4c4f8d0dd2
Milic Vojinovic <milic.vojinovic@gmail.com>
2020-08-05 13:28:37 +0200

    Modify the cart presentation logic

--- src/services/cart/cart.service.ts
+++ src/services/cart/cart.service.ts
@@ -1,12 +1,10 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { Administrator } from "src/entities/administrator.entity";
 import { Repository } from "typeorm";
 import { Cart } from "src/entities/cart.entity";
 import { CartArticle } from "src/entities/cart-article.entity";
 import { Article } from "src/entities/article.entity";
 import { Order } from "src/entities/order.entity";
-import { createWriteStream } from "fs";
 
 @Injectable()
 export class CartService {
@@ -15,17 +13,12 @@ export class CartService {
         private readonly cart: Repository<Cart>,
 
         @InjectRepository(CartArticle)
-        private readonly cartArticle: Repository<CartArticle>,
-
-        @InjectRepository(Article)
-        private readonly article: Repository<Article>,
-
-        @InjectRepository(Order)
-        private readonly order: Repository<Order>
-    ) { }
+        private readonly cartArticle: Repository<CartArticle>    ) { }
 
 
+    // get last active (no order is placed and no 0 quantity of articles) cart for active user
     async getLastActiveCartByUserId(userId: number): Promise<Cart | null> {
+        //get array of carts from repository,with userId from active user
         const carts = await this.cart.find({
             where: {
                 userId: userId
@@ -37,12 +30,14 @@ export class CartService {
             relations: ["order"],
         });
 
+        // check if there is cart for active user
         if (!carts || carts.length === 0) {
             return null;
         }
-
+        // get last created cart from carts array ;  "DESC" created at command in query
         const cart = carts[0];
 
+        // check if there is order on that last created cart
         if (cart.order !== null) {
             return null;
         }
@@ -50,6 +45,7 @@ export class CartService {
         return cart;
     }
 
+    // create new cart for active user
     async createNewCartForUser(userId: number): Promise<Cart> {
         const newCart: Cart = new Cart();
         newCart.userId = userId;
@@ -57,24 +53,29 @@ export class CartService {
         return await this.cart.save(newCart);
     }
 
+    // add article to cart in cart_article table
     async addArticleToCart(cartId: number, articleId: number, quantity: number): Promise<Cart> {
-        // get Cart Article data 
+        // get Cart Article data(row) for cart id and desired article id
         let record: CartArticle = await this.cartArticle.findOne({
             cartId: cartId,
             articleId: articleId,
         })
 
-        // check if Cart Article data exist
+        // check if Cart Article data(row) for desired article already exist ;
         if (!record) {
+            // if not create new row for article with article id 
             record = new CartArticle();
             record.cartId = cartId;
             record.articleId = articleId;
             record.quantity = quantity;
 
             record = await this.cartArticle.save(record);
+
         } else {
+            // Cart Article data(row) exist => just change quantity 
             record.quantity += quantity;
         }
+
 
         await this.cartArticle.save(record);
 
@@ -82,26 +83,21 @@ export class CartService {
 
     }
 
-    async getById(cartId: number): Promise<Cart> {
-        return await this.cart.findOne(cartId, {
-            relations: [
-                "user",
-                "cartArticles",
-                "cartArticles.article",
-                "cartArticles.article.category",
-            ]
-        });
-    }
-
+   
+    // change quantity of article in cart_article table
     async changeQuantity(cartId : number , articleId : number , newQuantity: number ): Promise<Cart>{
+        // get Cart Article data(row) for cart id and article id 
         let record: CartArticle = await this.cartArticle.findOne({
             cartId: cartId,
             articleId: articleId,
         });
 
+         // check if Cart Article data(row) exist ;
         if(record) {
             record.quantity = newQuantity;
 
+            // if quantity is change to 0 we delete that article from active cart 
+            // else we save active cart with new value for quantity
             if(record.quantity ===0){
                 await this.cartArticle.delete(record.cartArticleId)
             } else {
@@ -110,6 +106,21 @@ export class CartService {
         }
 
         return await this.getById(cartId);
+    }
+
+
+    // get cart and related tables
+    async getById(cartId: number): Promise<Cart> {
+        return await this.cart.findOne(cartId, {
+            relations: [
+                "user",
+                "cartArticles",
+                "cartArticles.article",
+                "cartArticles.article.category",
+                "cartArticles.article.articlePrices",
+
+            ]
+        });
     }
 
 
